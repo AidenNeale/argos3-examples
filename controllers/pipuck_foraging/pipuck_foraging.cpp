60f6105285f25f69a2ac17334329f528665ebf85
AidenNeale <aidenneale@sky.com>
2022-08-01 13:03:33 +0100

    Initial Commit for foraging - currently runs a diffusion example. Pushed for completeness

---
+++ controllers/pipuck_foraging/pipuck_foraging.cpp
@@ -0,0 +1,160 @@
+/* Include the controller definition */
+#include "pipuck_foraging.h"
+/* Function definitions for XML parsing */
+#include <argos3/core/utility/configuration/argos_configuration.h>
+
+/****************************************/
+/****************************************/
+
+CPiPuckForaging::CPiPuckForaging() :
+   pcWheels(NULL),
+   pcGround(NULL),
+   pcProximity(NULL),
+   m_fWheelVelocity(2.5f) {}
+
+/****************************************/
+/****************************************/
+
+void CPiPuckForaging::Init(TConfigurationNode& t_node) {
+   /*
+    * Get sensor/actuator handles
+    *
+    * The passed string (ex. "differential_steering") corresponds to the
+    * XML tag of the device whose handle we want to have. For a list of
+    * allowed values, type at the command prompt:
+    *
+    * $ argos3 -q actuators
+    *
+    * to have a list of all the possible actuators, or
+    *
+    * $ argos3 -q sensors
+    *
+    * to have a list of all the possible sensors.
+    *
+    * NOTE: ARGoS creates and initializes actuators and sensors
+    * internally, on the basis of the lists provided the configuration
+    * file at the <controllers><epuck_Foraging><actuators> and
+    * <controllers><epuck_Foraging><sensors> sections. If you forgot to
+    * list a device in the XML and then you request it here, an error
+    * occurs.
+    */
+    pcWheels = GetActuator<CCI_PiPuckDifferentialDriveActuator>("pipuck_differential_drive");
+    pcGround = GetSensor<CCI_PiPuckGroundColourSensor>("pipuck_ground_colour");
+    pcProximity = GetSensor<CCI_PiPuckRangefindersSensor>("pipuck_rangefinders");
+   /*
+    * Parse the configuration file
+    *
+    * The user defines this part. Here, the algorithm accepts three
+    * parameters and it's nice to put them in the config file so we don't
+    * have to recompile if we want to try other settings.
+    */
+   GetNodeAttributeOrDefault(t_node, "velocity", m_fWheelVelocity, m_fWheelVelocity);
+}
+
+/****************************************/
+/****************************************/
+
+void CPiPuckForaging::ControlStep() {
+  pcWheels->SetLinearVelocity(m_fWheelVelocity, m_fWheelVelocity);
+  std::vector<Real> vecReadings;
+  Real Readings[8];
+  pcProximity->Visit([&vecReadings, &Readings] (const CCI_PiPuckRangefindersSensor::SInterface& s_interface)
+  {
+      vecReadings.emplace_back(s_interface.Proximity);
+      Readings[s_interface.Label] = s_interface.Proximity;
+
+      /*
+      These output phrases are useful for debugging and understanding the
+      example however cause the simulator to slow down to the sheer quantity of logging
+      performed with 50 pipucks (8 messages per range finder sensor per robot)
+      */
+
+      // std::cout << "Label: " << s_interface.Label << std::endl;
+      // std::cout << "Proximity: " << s_interface.Proximity << std::endl;
+
+      // const CCI_PiPuckRangefindersSensor::TConfiguration configuration = s_interface.Configuration;
+      // std::cout << "Configuration:" << std::endl;
+      // std::cout << "Name: " << std::get<0>(configuration) << std::endl;
+      // std::cout << "Position: " << std::get<1>(configuration) << std::endl;
+      // std::cout << "Orientation: " << std::get<2>(configuration) << std::endl;
+      // std::cout << "Range: " << std::get<3>(configuration) << std::endl;
+
+      // std::cout << std::endl;
+  });
+  Real left = 0.5;
+  Real right = 0.5;
+
+  if(Readings[0] < 0.1)
+  {
+    if(Readings[7] < 0.1)
+    {
+      left = -0.2;
+      right = 0.2;
+    }
+    else
+    {
+      if(Readings[1] < 0.1)
+      {
+        if(Readings[2] < 0.1)
+        {
+          left = -0.1;
+          right = 0.2;
+        }
+        else
+        {
+          left = -0.1;
+          right = 0.1;
+        }
+      }
+      else
+      {
+        left = 0.0;
+        right = 0.2;
+      }
+    }
+  }
+  else if(Readings[7] < 0.1)
+  {
+    if(Readings[6] < 0.1)
+    {
+      if(Readings[5] < 0.1)
+      {
+        left = 0.2;
+        right = -0.1;
+      }
+      else
+      {
+        left = 0.1;
+        right = -0.1;
+      }
+    }
+    else
+    {
+      right = 0.0;
+      left = 0.2;
+    }
+  }
+  else
+  {
+    left = 0.5;
+    right = 0.5;
+  }
+
+
+  pcWheels->SetLinearVelocity(left, right);
+}
+
+/****************************************/
+/****************************************/
+
+/*
+ * This statement notifies ARGoS of the existence of the controller.
+ * It binds the class passed as first argument to the string passed as
+ * second argument.
+ * The string is then usable in the configuration file to refer to this
+ * controller.
+ * When ARGoS reads that string in the configuration file, it knows which
+ * controller class to instantiate.
+ * See also the configuration files for an example of how this is used.
+ */
+REGISTER_CONTROLLER(CPiPuckForaging, "pipuck_foraging_controller")
---
+++ controllers/pipuck_foraging/pipuck_foraging.h
@@ -0,0 +1,98 @@
+/*
+ * AUTHOR: Carlo Pinciroli <cpinciro@ulb.ac.be>
+ *
+ * An example controller for obstacle avoidance with the e-puck.
+ *
+ * The controller uses the proximity sensor to detect obstacles and the
+ * wheels to move the robot around.
+ *
+ * This controller is meant to be used with the XML files:
+ *    experiments/pipuck_Foraging.argos
+ */
+
+#ifndef PIPUCK_FORAGING_H
+#define PIPUCK_FORAGING_H
+
+/*
+ * Include some necessary headers.
+ */
+/* Definition of the CCI_Controller class. */
+#include <argos3/core/control_interface/ci_controller.h>
+/* Definition of the differential steering actuator */
+#include <argos3/plugins/robots/pi-puck/control_interface/ci_pipuck_differential_drive_actuator.h>
+/* Definition of the Pipuck Rangefinders sensor */
+#include <argos3/plugins/robots/pi-puck/control_interface/ci_pipuck_rangefinders_sensor.h>
+/* Definition of the Pipuck Ground Colour sensor */
+#include <argos3/plugins/robots/pi-puck/control_interface/ci_pipuck_ground_colour_sensor.h>
+
+/*
+ * All the ARGoS stuff in the 'argos' namespace.
+ * With this statement, you save typing argos:: every time.
+ */
+using namespace argos;
+
+/*
+ * A controller is simply an implementation of the CCI_Controller class.
+ */
+class CPiPuckForaging : public CCI_Controller {
+
+public:
+
+   /* Class constructor. */
+   CPiPuckForaging();
+
+   /* Class destructor. */
+   virtual ~CPiPuckForaging() {}
+
+   /*
+    * This function initializes the controller.
+    * The 't_node' variable points to the <parameters> section in the XML
+    * file in the <controllers><pipuck_foraging_controller> section.
+    */
+   virtual void Init(TConfigurationNode& t_node);
+
+   /*
+    * This function is called once every time step.
+    * The length of the time step is set in the XML file.
+    */
+   virtual void ControlStep();
+
+   /*
+    * This function resets the controller to its state right after the
+    * Init().
+    * It is called when you press the reset button in the GUI.
+    * In this example controller there is no need for resetting anything,
+    * so the function could have been omitted. It's here just for
+    * completeness.
+    */
+   virtual void Reset() {}
+
+   /*
+    * Called to cleanup what done by Init() when the experiment finishes.
+    * In this example controller there is no need for clean anything up,
+    * so the function could have been omitted. It's here just for
+    * completeness.
+    */
+   virtual void Destroy() {}
+
+private:
+
+   /* Pointer to the differential steering actuator */
+   CCI_PiPuckDifferentialDriveActuator* pcWheels;
+   /* Pointer to the pi-puck proximity sensor */
+   CCI_PiPuckRangefindersSensor* pcProximity;
+   /* Pointer to the pi-puck ground colour sensor */
+   CCI_PiPuckGroundColourSensor* pcGround;
+
+   /*
+    * The following variables are used as parameters for the
+    * algorithm. You can set their value in the <parameters> section
+    * of the XML configuration file, under the
+    * <controllers><pipuck_foraging_controller> section.
+    */
+   /* Wheel speed. */
+   Real m_fWheelVelocity;
+
+};
+
+#endif
