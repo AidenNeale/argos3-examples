f16c765a2385a64aad34567e703a42a4727a5afa
AidenNeale <aidenneale@sky.com>
2022-08-03 19:18:26 +0100

    Changed the way in which floor zones were created to make them OO focused to enable future functionality of zone disappearance

--- loop_functions/pipuck_foraging_loop_functions/pipuck_foraging_loop_functions.cpp
+++ loop_functions/pipuck_foraging_loop_functions/pipuck_foraging_loop_functions.cpp
@@ -13,10 +13,6 @@ CPiPuckForagingLoopFunctions::CPiPuckForagingLoopFunctions() :
   m_cForagingArenaSideY(-1.7f, 1.7f),
   m_pcFloor(NULL),
   m_pcRNG(NULL)
-  // m_unCollectedFood(0),
-  // m_nEnergy(0),
-  // m_unEnergyPerFoodItem(1),
-  // m_unEnergyPerWalkingRobot(1)
   {}
 
   /****************************************/
@@ -31,17 +27,22 @@ void CPiPuckForagingLoopFunctions::Init(TConfigurationNode& t_node) {
   GetNodeAttribute(tForaging, "foodZones", numFoodZones);
   GetNodeAttribute(tForaging, "waterZones", numWaterZones);
   GetNodeAttribute(tForaging, "zoneRadius", zoneRadius);
+  GetNodeAttribute(tForaging, "zoneLifetime", zoneLifetime);
 
   /* Setup default starting values for food and water position */
   for (int i = 0; i < numFoodZones; i++) {
-    m_cFoodPos.push_back(CVector2(1000, 1000));
+    m_foodZones.push_back(SZoneData());
+    m_foodZones[i].zoneId = i;
+    m_foodZones[i].lifetime = zoneLifetime;
   }
   for (int i = 0; i < numWaterZones; i++) {
-    m_cWaterPos.push_back(CVector2(1000, 1000));
+    m_waterZones.push_back(SZoneData());
+    m_waterZones[i].zoneId = i;
+    m_waterZones[i].lifetime = zoneLifetime;
   }
   /* Distribute uniformly the items in the environment */
-  DistributeFoodSource();
-  DistributeWaterSource();
+  DistributeFoodSource(0, numFoodZones);
+  DistributeWaterSource(0, numWaterZones);
   foodLocationDefined = false;
   waterLocationDefined = false;
 }
@@ -49,55 +50,61 @@ void CPiPuckForagingLoopFunctions::Init(TConfigurationNode& t_node) {
   /****************************************/
   /****************************************/
 
-void CPiPuckForagingLoopFunctions::DistributeWaterSource() {
-  bool locationOverlap, waterOverlapDetected;
+void CPiPuckForagingLoopFunctions::DistributeWaterSource(int startIndex, int endIndex) {
+  bool locationOverlap, foodOverlapDetected, waterOverlapDetected;
   CVector2 newPos;
-  for (int waterIndex = 0; waterIndex < numWaterZones; waterIndex++) {
+  for (int waterIndex = startIndex; waterIndex < endIndex; waterIndex++) {
     locationOverlap = true;
     while (locationOverlap) {
+      foodOverlapDetected = false;
       waterOverlapDetected = false;
       newPos = CVector2(m_pcRNG->Uniform(m_cForagingArenaSideX),
                         m_pcRNG->Uniform(m_cForagingArenaSideY));
       /* Loop through previous water zones and check for overlaps */
-      for (int oldWaterIndex=0; oldWaterIndex < waterIndex; oldWaterIndex++) {
-        if ((newPos - m_cWaterPos[oldWaterIndex]).Length() <= (2 * zoneRadius)) {
+      for (int oldWaterIndex=0; oldWaterIndex < numWaterZones; oldWaterIndex++) {
+        if (oldWaterIndex == waterIndex) {
+          // Do nothing, don't check against itself
+        }
+        if ((newPos - m_waterZones[oldWaterIndex].zoneLocation).Length() <= (2 * zoneRadius)) {
           waterOverlapDetected = true;
           break;
         }
       }
       /* Loop through food zones and check for overlap */
       for (int foodIndex = 0; foodIndex < numFoodZones; foodIndex++) {
-        if ((newPos - m_cFoodPos[foodIndex]).Length() <= (2 * zoneRadius)) {
+        if ((newPos - m_foodZones[foodIndex].zoneLocation).Length() <= (2 * zoneRadius)) {
+          foodOverlapDetected = true;
           break;
         }
-        if ((foodIndex == (numFoodZones - 1)) && !waterOverlapDetected) {
-          locationOverlap = false;
-        }
       }
-      if (numFoodZones == 0 && !waterOverlapDetected) {
+      /* If no overlaps detected, break from loop */
+      if (!foodOverlapDetected && !waterOverlapDetected) {
         locationOverlap = false;
       }
     }
-    m_cWaterPos[waterIndex].Set(newPos.GetX(), newPos.GetY());
-
+    m_waterZones[waterIndex].zoneLocation.Set(newPos.GetX(), newPos.GetY());
   }
 }
 
   /****************************************/
   /****************************************/
 
-void CPiPuckForagingLoopFunctions::DistributeFoodSource() {
-  bool foodOverlapDetected, locationOverlap;
+void CPiPuckForagingLoopFunctions::DistributeFoodSource(int startIndex, int endIndex) {
+  bool foodOverlapDetected, waterOverlapDetected, locationOverlap;
   CVector2 newPos;
-  for (int foodIndex = 0; foodIndex < numFoodZones; foodIndex++) {
+  for (int foodIndex = startIndex; foodIndex < endIndex; foodIndex++) {
     locationOverlap = true;
     while (locationOverlap) {
-    foodOverlapDetected = false;
+      foodOverlapDetected = false;
+      waterOverlapDetected = false;
       newPos = CVector2(m_pcRNG->Uniform(m_cForagingArenaSideX),
                         m_pcRNG->Uniform(m_cForagingArenaSideY));
       /* Loop through previous food zones and check for overlaps */
-      for (int oldFoodIndex=0; oldFoodIndex < foodIndex; oldFoodIndex++) {
-        if ((newPos - m_cFoodPos[oldFoodIndex]).Length() <= (2 * zoneRadius)) {
+      for (int oldFoodIndex=0; oldFoodIndex < numFoodZones; oldFoodIndex++) {
+        if (oldFoodIndex == foodIndex) {
+          // Do nothing, don't check against itself
+        }
+        else if ((newPos - m_foodZones[oldFoodIndex].zoneLocation).Length() <= (2 * zoneRadius)) {
           foodOverlapDetected = true;
           break;
         }
@@ -105,33 +112,39 @@ void CPiPuckForagingLoopFunctions::DistributeFoodSource() {
 
       /* Loop through water zones and check for overlaps */
       for (int waterIndex=0; waterIndex< numWaterZones; waterIndex++) {
-        if ((newPos - m_cWaterPos[waterIndex]).Length() <= (2 * zoneRadius)) {
+        if ((newPos - m_waterZones[waterIndex].zoneLocation).Length() <= (2 * zoneRadius)) {
+          waterOverlapDetected = true;
           break;
         }
-        if ((waterIndex == (numWaterZones - 1)) && !foodOverlapDetected) {
-          locationOverlap = false;
-        }
       }
-      if (numWaterZones == 0 && !foodOverlapDetected) {
+      /* If no overlaps detected, break from loop */
+      if (!waterOverlapDetected && !foodOverlapDetected) {
         locationOverlap = false;
       }
     }
-    m_cFoodPos[foodIndex].Set(newPos.GetX(), newPos.GetY());
+    m_foodZones[foodIndex].zoneLocation.Set(newPos.GetX(), newPos.GetY());
   }
 }
 
   /****************************************/
   /****************************************/
 
+  // void setNewZone(int index, str typeOfZone) {
+
+  // }
+
+  /****************************************/
+  /****************************************/
+
 void CPiPuckForagingLoopFunctions::Reset() {
   for (int i = 0; i < numFoodZones; i++) {
-    m_cFoodPos[i].Set(1000, 1000);
+    m_foodZones[i].zoneLocation.Set(1000, 1000);
   }
   for (int i = 0; i < numWaterZones; i++) {
-    m_cWaterPos[i].Set(1000, 1000);
+    m_waterZones[i].zoneLocation.Set(1000, 1000);
   }
-  DistributeFoodSource();
-  DistributeWaterSource();
+  DistributeFoodSource(0, numFoodZones);
+  DistributeWaterSource(0, numWaterZones);
 }
 
   /****************************************/
@@ -148,13 +161,13 @@ CColor CPiPuckForagingLoopFunctions::GetFloorColor(const CVector2& c_position_on
   if(c_position_on_plane.GetX() < -1.5f) {
     return CColor::ORANGE;
   }
-  for(UInt32 i = 0; i < m_cFoodPos.size(); i++) {
-    if((c_position_on_plane - m_cFoodPos[i]).Length() < zoneRadius) {
+  for(UInt32 i = 0; i < m_foodZones.size(); i++) {
+    if((c_position_on_plane - m_foodZones[i].zoneLocation).Length() < zoneRadius) {
       return CColor::RED;
     }
   }
-  for(UInt32 j = 0; j < m_cWaterPos.size(); j++) {
-    if((c_position_on_plane - m_cWaterPos[j]).Length() < zoneRadius) {
+  for(UInt32 j = 0; j < m_waterZones.size(); j++) {
+    if((c_position_on_plane - m_waterZones[j].zoneLocation).Length() < zoneRadius) {
       return CColor::BLUE;
     }
   }
@@ -171,7 +184,7 @@ void CPiPuckForagingLoopFunctions::PreStep() {
       CPiPuckEntity& cPipuck = *any_cast<CPiPuckEntity*>(it->second);
       CPiPuckForaging& cController = dynamic_cast<CPiPuckForaging&>(cPipuck.GetControllableEntity().GetController());
 
-
+      std::cout << cController.getGroundColor() << std::endl;
   }
 }
 
--- loop_functions/pipuck_foraging_loop_functions/pipuck_foraging_loop_functions.h
+++ loop_functions/pipuck_foraging_loop_functions/pipuck_foraging_loop_functions.h
@@ -24,21 +24,35 @@ namespace argos {
     virtual void Init(TConfigurationNode& t_tree);
     virtual void Reset();
     virtual void Destroy();
-    virtual void DistributeWaterSource();
-    virtual void DistributeFoodSource();
+    virtual void DistributeWaterSource(int, int);
+    virtual void DistributeFoodSource(int, int);
     virtual CColor GetFloorColor(const CVector2& c_position_on_plane);
     virtual void PreStep();
+
+    struct SZoneData {
+      int zoneId;
+      int lifetime;
+      CVector2 zoneLocation;
+
+      /* Default Constructor */
+      SZoneData() {
+        zoneId = -1;
+        lifetime = -1;
+        zoneLocation = CVector2(1000, 1000);
+      };
+
+      void Reset();
+    };
 
   private:
     CRange<Real> m_cForagingArenaSideX, m_cForagingArenaSideY;
     CFloorEntity* m_pcFloor;
     CRandom::CRNG* m_pcRNG;
 
-
-    std::vector<CVector2> m_cFoodPos;
-    std::vector<CVector2> m_cWaterPos;
+    std::vector<SZoneData> m_foodZones;
+    std::vector<SZoneData> m_waterZones;
     UInt32 numFoodZones, numWaterZones;
-    float zoneRadius;
+    float zoneRadius, zoneLifetime;
     bool foodLocationDefined, waterLocationDefined;
   };
 }
